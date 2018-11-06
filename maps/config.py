5f462dc164efc39e3cd425242e8c9a2ad154ec53
Josh Humphries <j.humphries@nhm.ac.uk>
2018-11-06 11:15:27 +0000

    Update comment to reflect reality

--- maps/config.py
+++ maps/config.py
@@ -2,8 +2,9 @@
 
 """
 This module contains default configuration options which will be read by flask on start. You can
-override them on an application level basis by using a file called `maps.cfg` placed in the flask
-instance folder.
+override them by using creating a configuration file and then pointing the environment variable
+"maps_config" at it. Make sure to use the full path. The configuration file can just be a python
+file like this one.
 """
 
 ELASTICSEARCH_HOSTS = ['http://localhost:9200']
