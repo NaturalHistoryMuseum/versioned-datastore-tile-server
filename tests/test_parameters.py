6a5e870fa8efada2232643b0cc37ea2c4cefc663
Josh Humphries <j.humphries@nhm.ac.uk>
2018-11-10 14:59:23 +0000

    Add extract tests

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python3
+from unittest.mock import MagicMock
 
 import pytest
 from colour import Color
 
 from maps.exceptions import InvalidColour
-from maps.parameters import parse_colour
+from maps.parameters import parse_colour, extract
+
+
+class TestExtract:
+
+    def test_extract_param_is_present_no_parser(self, monkeypatch):
+        args = {'test': 5.4}
+        monkeypatch.setattr('maps.parameters.request', MagicMock(args=args))
+        assert extract('test', default=None, parser=None) == 5.4
+
+    def test_extract_param_is_present_with_parser(self, monkeypatch):
+        args = {'test': 5.4}
+        monkeypatch.setattr('maps.parameters.request', MagicMock(args=args))
+        assert extract('test', default=None, parser=int) == 5
+
+    def test_extract_param_is_not_present_no_parser(self, monkeypatch):
+        args = {'test': 5.4}
+        monkeypatch.setattr('maps.parameters.request', MagicMock(args=args))
+        assert extract('not_test', default=6.1, parser=None) == 6.1
+
+    def test_extract_param_is_not_present_with_parser(self, monkeypatch):
+        args = {'test': 5.4}
+        monkeypatch.setattr('maps.parameters.request', MagicMock(args=args))
+        assert extract('not_test', default=6.1, parser=int) == 6.1
 
 
 def test_parse_colour():
